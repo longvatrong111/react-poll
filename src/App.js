f836080299dcdfa98143a190df96f48ed93313a4
longvatrong111 <longvatrong111@gmail.com>
2024-06-13 22:52:28 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+import Login from './views/Login.js';
+import Home from './views/Home.js';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='.App'>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" exact component={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route index path="/Login" component={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
